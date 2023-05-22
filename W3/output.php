9b7b3f6a3080f0e239dc28719942a687622e660e
prathamagrawal5 <126439341+prathamagrawal5@users.noreply.github.com>
2023-05-23 01:32:49 +0530

    Add files via upload

---
+++ W3/output.php
@@ -0,0 +1,11 @@
+<h1>AJAX POST METHOD</h1>
+<?php
+
+$username = $_POST['username'];
+$email = $_POST['email'];
+
+echo "<div> Hi,".$username."</div>";
+echo "<div>Your email is: ".$email."</div>";
+
+
+?>
